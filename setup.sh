0ad25ab3f2d3d148974a858ee6170efec5168391
Dru Still <eugene_still@brown.edu>
2025-04-04 13:58:52 -0400

    Full functionality

--- setup.sh
+++ setup.sh
@@ -22,29 +22,29 @@ else
   echo "Fisher is already installed."
 fi
 
-# Install nvm.fish if not installed
-# if ! fish -c 'functions -q nvm'
-# then
-#   echo "Installing nvm.fish via Fisher..."
-#   fish -c 'fisher install jorgebucaran/nvm.fish'
-# else
-#   echo "nvm.fish already installed."
-# fi
+Install nvm.fish if not installed
+if ! fish -c 'functions -q nvm'
+then
+  echo "Installing nvm.fish via Fisher..."
+  fish -c 'fisher install jorgebucaran/nvm.fish'
+else
+  echo "nvm.fish already installed."
+fi
 
-# # Rust + cargo
-# if [ ! -f "$HOME/.cargo/env" ]; then
-#   echo "Installing Rust and cargo..."
-#   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-#   source "$HOME/.cargo/env"
-# else
-#   echo "Rust already installed."
-# fi
+# Rust + cargo
+if [ ! -f "$HOME/.cargo/env" ]; then
+  echo "Installing Rust and cargo..."
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+  source "$HOME/.cargo/env"
+else
+  echo "Rust already installed."
+fi
 
-# # starship
-# if ! command -v starship &> /dev/null; then
-#   echo "Installing starship..."
-#   mkdir -p ~/.local/bin
-#   curl -sS https://starship.rs/install.sh | sh -s -- -b ~/.local/bin -y
-# else
-#   echo "Starship already installed."
-# fi
+# starship
+if ! command -v starship &> /dev/null; then
+  echo "Installing starship..."
+  mkdir -p ~/.local/bin
+  curl -sS https://starship.rs/install.sh | sh -s -- -b ~/.local/bin -y
+else
+  echo "Starship already installed."
+fi
