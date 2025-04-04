4ca8c5c323fbb6efb723d86db7710a70fc23bcbf
Dru Still <eugene_still@brown.edu>
2025-04-04 13:57:46 -0400

    fisher

--- setup.sh
+++ setup.sh
@@ -17,42 +17,34 @@ fi
 if ! fish -c 'functions -q fisher'
 then
   echo "Fisher not found. Installing..."
-  curl -sL https://git.io/fisher | fish
+  fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher'
 else
   echo "Fisher is already installed."
 fi
 
 # Install nvm.fish if not installed
-if ! fish -c 'functions -q nvm'
-then
-  echo "Installing nvm.fish via Fisher..."
-  fish -c 'fisher install jorgebucaran/nvm.fish'
-else
-  echo "nvm.fish already installed."
-fi
+# if ! fish -c 'functions -q nvm'
+# then
+#   echo "Installing nvm.fish via Fisher..."
+#   fish -c 'fisher install jorgebucaran/nvm.fish'
+# else
+#   echo "nvm.fish already installed."
+# fi
 
-# nvm
-if [ ! -d "$HOME/.nvm" ]; then
-  echo "Installing nvm..."
-  fish -c "fisher install jorgebucaran/nvm.fish"
-else
-  echo "nvm already installed."
-fi
+# # Rust + cargo
+# if [ ! -f "$HOME/.cargo/env" ]; then
+#   echo "Installing Rust and cargo..."
+#   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+#   source "$HOME/.cargo/env"
+# else
+#   echo "Rust already installed."
+# fi
 
-# Rust + cargo
-if [ ! -f "$HOME/.cargo/env" ]; then
-  echo "Installing Rust and cargo..."
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-  source "$HOME/.cargo/env"
-else
-  echo "Rust already installed."
-fi
-
-# starship
-if ! command -v starship &> /dev/null; then
-  echo "Installing starship..."
-  mkdir -p ~/.local/bin
-  curl -sS https://starship.rs/install.sh | sh -s -- -b ~/.local/bin -y
-else
-  echo "Starship already installed."
-fi
+# # starship
+# if ! command -v starship &> /dev/null; then
+#   echo "Installing starship..."
+#   mkdir -p ~/.local/bin
+#   curl -sS https://starship.rs/install.sh | sh -s -- -b ~/.local/bin -y
+# else
+#   echo "Starship already installed."
+# fi
